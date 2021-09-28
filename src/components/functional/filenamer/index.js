19bf38fe733441444d53687e4cd94e29563fd40a
Daniel Soto <dasg.1993@gmail.com>
2021-09-27 20:06:19 -0500

    [feature] - conditional rendering & synthetics events

--- src/components/functional/filenamer/index.js
+++ src/components/functional/filenamer/index.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 const FileNamer = () => {
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState(false);
+
+    const validate = (event) => {
+        if(/\*/.test(name)) {
+            event.preventDefault();
+            setMessage(true);
+            return;
+        }
+        setMessage(false);
+    }
     return (
         <div className="wrapper">
             <div className="preview">
-                <h2>Preview:</h2>
+                {name && (<h2>Preview: {name}.js</h2>)}
             <form>
                 <label>
                     <p>Name</p>
-                    <input type="text" name="name" />
+                    <input 
+                        type="text" name="name"
+                        autoComplete="off" 
+                        onChange={event => setName(event.target.value)}
+                        onFocus={() => setMessage(true)}
+                        onBlur={() => setMessage(false)}
+                        />
                 </label>
+                {message && (
+                    <div>
+                        <span role="img" aria-label="allowed">✅</span> Alphanumeric characters
+                        <br />
+                        <span role="img" aria-label="not allowed">⛔️</span> No asterisk (*)
+                    </div>
+                )}
                 <div className="button-container">
-                    <button>Save</button>
+                    <button onClick={validate}>Save</button>
                 </div>
             </form>
             </div>
