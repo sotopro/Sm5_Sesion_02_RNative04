c9f591debd28bcacb3b7e568ec0691eb804a1a9a
Daniel Soto <dasg.1993@gmail.com>
2021-09-27 21:02:02 -0500

    [feature] - isEmpty

--- src/components/functional/filenamer/index.js
+++ src/components/functional/filenamer/index.js
@@ -1,18 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 const FileNamer = () => {
     const [name, setName] = useState('');
     const [message, setMessage] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+
+    useEffect(() => {
+        const handleWindowClick = () => {
+            setMessage(false);
+            setIsEmpty(false);
+        }
+        if(message || isEmpty) {
+            window.addEventListener('click', handleWindowClick);
+        } else {
+            window.removeEventListener('click', handleWindowClick);
+        }
+
+        return () => window.removeEventListener('click', handleWindowClick);
+    }, [message,setMessage, setIsEmpty, isEmpty]);
 
     const validate = (event) => {
-        if(/\*/.test(name)) {
-            event.preventDefault();
-            setMessage(true);
+        event.preventDefault();
+        if(event.target.value.length === 0) {
+            setIsEmpty(true);
             return;
+        } else {
+            if(/\*/.test(name)) {
+                setMessage(true);
+                return;
+            }
+            setMessage(false);
         }
-        setMessage(false);
     }
+
+    const handleChange = (event) => {
+        setName(event.target.value);
+        setIsEmpty(event.target.value.length === 0);
+    }
+
     return (
         <div className="wrapper">
             <div className="preview">
@@ -23,20 +49,32 @@ const FileNamer = () => {
                     <input 
                         type="text" name="name"
                         autoComplete="off" 
-                        onChange={event => setName(event.target.value)}
-                        onFocus={() => setMessage(true)}
-                        onBlur={() => setMessage(false)}
+                        onChange={event => handleChange(event)}
                         />
                 </label>
-                {message && (
-                    <div>
+                <div className="information-wrapper">
+                    <button 
+                        type="button"
+                        className="information"
+                        onClick={() => setMessage(true)}
+                    >
+                        more information
+                    </button>
+                </div>
+                {(message || isEmpty) && (
+                    <div className="popup">
                         <span role="img" aria-label="allowed">✅</span> Alphanumeric characters
                         <br />
                         <span role="img" aria-label="not allowed">⛔️</span> No asterisk (*)
                     </div>
                 )}
+                {isEmpty && (
+                    <div className="popup">
+                    <span role="img" aria-label="required">⛔️</span> field required
+                </div>
+                )}
                 <div className="button-container">
-                    <button onClick={validate}>Save</button>
+                    <button type="submit" onClick={validate} className="save">Save</button>
                 </div>
             </form>
             </div>
